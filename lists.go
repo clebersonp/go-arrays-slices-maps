09cf7c475dd3f5cc32bb2e22da58934abd331df0
Cleberson <pauluci.cleberson@gmail.com>
2024-02-13 15:39:53 -0300

    Go array syntax with declaration and initialization

--- lists.go
+++ lists.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// array syntax: declaration and initialization: [num of elements]type{initialize values}
+	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	fmt.Printf("Price Type: %T, Prices: %v\n", prices, prices)
 }

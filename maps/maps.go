feb2bd8df4d877142966936e4f8e6d290893c43c
Cleberson <pauluci.cleberson@gmail.com>
2024-02-13 20:15:30 -0300

    Using maps and some operations like adding, removing etc..

--- maps/maps.go
+++ maps/maps.go
@@ -12,4 +12,16 @@ func main() {
 	}
 	fmt.Printf("Websites type: %T, content: %v\n", websites, websites)
 
+	// adding a new key-value to maps in Go is so easy, it's dynamic
+	websites["LinkedIn"] = "https://linkedin.com"
+	fmt.Printf("After added a new key-value: %v\n", websites)
+
+	// remove a key-value from map with a built-in delete(map, key) function
+	delete(websites, "Google")
+	fmt.Printf("After delete a key-value: %v\n", websites)
+
+	// there is no error if you try to delete a key that does not exist in map
+	delete(websites, "some-key")
+	fmt.Printf("After try delete a key-value that does not exists: %v\n", websites)
+
 }

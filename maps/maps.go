26e0a2763947ae8cd64dd239d9ea8dafbd08560d
Cleberson <pauluci.cleberson@gmail.com>
2024-02-13 19:41:48 -0300

    Created the first map example

--- maps/maps.go
+++ maps/maps.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 // to run this application in terminal: go run maps.go
 
 func main() {
+	// map syntax: map[keyType]valueType{}
+	websites := map[string]string{
+		"Google":              "https://google.com",
+		"Amazon Web Services": "https://aws.com",
+	}
+	fmt.Printf("Websites type: %T, content: %v\n", websites, websites)
 
 }

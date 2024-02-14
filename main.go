5b39426799e7f976f29127c491da9e4f9691bf9d
Cleberson <pauluci.cleberson@gmail.com>
2024-02-13 21:38:02 -0300

    Created a type alias to explain about it

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// Go feature called "type alias"
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 
 	// function make to create a slice of string with initial length of 2 and a capacity of max 5 elements
@@ -17,10 +24,10 @@ func main() {
 
 	fmt.Println("--------------------------")
 	// unlike make with slices, with map has only one int argument for initial capacity
-	courseRatings := make(map[string]float64, 4)
+	courseRatings := make(floatMap, 4)
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 4.8
 	courseRatings["angular"] = 4.7
 
-	fmt.Println(courseRatings)
+	courseRatings.output()
 }
